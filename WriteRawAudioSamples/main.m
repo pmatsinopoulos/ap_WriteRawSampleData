384cad3d6f0aeabaaecdfd1cb13da4dd3789730b
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-03-25 07:19:45 +0200

    Initial Commit

---
+++ WriteRawAudioSamples/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WriteRawAudioSamples
+//
+//  Created by Panayotis Matsinopoulos on 25/3/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
